560e14c75fbb682ae04197cdcda1a87fc3ce9e7d
Aniruddh <adubge@gmail.com>
2023-10-25 18:58:54 +0530

    solbol

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,16 @@ import type { AppProps } from 'next/app'
 import { ThirdwebProvider, coinbaseWallet, metamaskWallet, phantomWallet, rainbowWallet, trustWallet, walletConnect, zerionWallet } from "@thirdweb-dev/react";
 import { ConnectWallet } from "@thirdweb-dev/react";
 
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (<div>
+  return (
+  <div>  
   <ThirdwebProvider
     activeChain="mumbai"
     autoSwitch
     autoConnect
-    secretKey={"En-DcKOlLOsImDlqFyuKbgAywPxya_gKOATT0E41O9X4v4HUkvxv-7GVrOWFu5efWjWPDm8I0VY2qgNEnkHz5A"}
+    secretKey="uDfkSfvNQ0O-6_YO5oIsr7IHta9IgyvpAUPVKQpOx0UC9MyxwFvU93u32r3lTtxzINeH9_8DNO1MvZTmh6uYYA"
     theme="dark"
-    clientId="af574d551a74949aacbf76fbee4f40f4"
+    clientId="3b4bf0a94bc9e7552249cd763a42613c"
     supportedWallets={[
       metamaskWallet(),
       coinbaseWallet(),
@@ -25,9 +25,12 @@ export default function App({ Component, pageProps }: AppProps) {
   >
   <Component {...pageProps} />;
   <ConnectWallet />
+  
+</ThirdwebProvider>
+</div>
+)
 
-</ThirdwebProvider></div>)
- 
+
 };
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useContractRead, useContract,  } from "@thirdweb-dev/react";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
-const inter = Inter({ subsets: ['latin'] })
-
+// If used on the FRONTEND pass your 'clientId'
+const sdk = new ThirdwebSDK("mumbai", {
+  clientId: "YOUR_CLIENT_ID",
+});
 export default function Home() {
+  // Your smart contract address
+  const contractAddress = "0x6EaA4739f5296FF9694a5a1831e9fb9c3be2D702";
+
+  const { contract } = useContract(contractAddress);
+
+  const { data, isLoading, error } = useContractRead(
+    contract,
+    "add",
+  );
+  console.log(data);
+
+  if (error) {
+    console.error("failed to read contract", error);
+  }
+
   return (
-    <> hi </>
-  )
+    <div>{isLoading ? <p>Loading...</p> : <p>Contract Name: {data.toString()}</p>}</div>
+  );
 }
+
